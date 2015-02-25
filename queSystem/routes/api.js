6400d6edd1f165003a190393e0bafac6cc2093cc
sigvartmh <sigvart.m@gmail.com>
2015-02-25 15:18:47 +0100

    Added jwt authentication for the API routes

--- queSystem/routes/api.js
+++ queSystem/routes/api.js
@@ -1,11 +1,72 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
 var User = require('../models/user');
+
 var router = express.Router();
+
+var tokenSecret = 'iloveyou';
+
+router.post('/authenticate', function(req, res){
+    console.log("test");
+    console.log(req.body);
+    User.findOne({
+        username: req.body.username
+    }).select('name username password').exec(function(err, user){
+        if(err) throw err;
+        if(!user){
+            res.json({
+                success: false,
+                message: 'Authentication failed. User not found.'
+            });
+        } else if(user){
+            var validPassword = user.isValidPassword(req.body.password);
+            if(!validPassword){
+                res.json({
+                    success: false,
+                    message: 'Authenticate failed. Wrong username or password.'
+                });
+            } else {
+                var token = jwt.sign({
+                    name: user.name,
+                    username: user.username
+                },
+                tokenSecret, {
+                    expiresInMinutes: 1440 //Expires in 24 hours(maybe this should be set in config?)
+                });
+
+                res.json({
+                    success: true,
+                    message: 'Here is your token! Enjoy the Que',
+                    token: token
+                });
+            }
+        }
+    });
+});
 
 /* GET home page. */
 router.use(function(req, res, next){
-    console.log("somebody came to our app!");
-    next();
+
+    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+    if(token){
+        jwt.verify(token, tokenSecret, function(err, decoded){
+            if(err){
+                return res.status(403).send({
+                    success: false,
+                    message: 'Failed to authenticate token.'
+                });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
 });
 
 router.get('/', function(req, res, next) {
@@ -15,6 +76,10 @@ router.get('/', function(req, res, next) {
 router.get('/test', function(req, res){
   res.json({ message: 'Welcome to the Que API!' });
 });
+
+router.get('/me', function(req, res){
+    res.send(req.decoded);
+})
 
 router.route('/users')
     .post(function(req, res){
@@ -65,7 +130,6 @@ router.route('/users/:user_id')
 
             user.save(function(err){
                 if(err) res.send(err);
-
                 res.json({ message: 'User updated!' });
             });
         });
